5f8783c23e6f3ac40e58e4deb68a769b158f1f8f
jason-maier-temp <jasonmaier45@gmail.com>
2024-04-02 11:41:22 -0700

    Add Message Card

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,27 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {message && <div>message</div>}
+      {message && (
+        <div className="flex flex-row items-start w-3/5">
+          <Image
+            src="./vultron.svg"
+            width={30}
+            height={30}
+            alt="Vultron Logo"
+          />
+          <div className="ml-4 flex flex-col">
+            <div className="font-bold">VultGPT</div>
+            <div>{message}</div>
+          </div>
+        </div>
+      )}
       {!message && (
         <div className="flex flex-col items-center my-auto">
           <Image
             src="./vultron.svg"
             width={80}
             height={80}
-            alt="VultGPT Logo"
+            alt="Vultron Logo"
           />
           <h2 className="my-4 text-3xl font-bold">How can I help you today?</h2>
         </div>
