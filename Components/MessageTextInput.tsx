00a2b76b0abc3f9800123546d1102b640a47e62b
jason-maier-temp <jasonmaier45@gmail.com>
2024-04-03 11:01:24 -0700

    Styling Tweaks

--- Components/MessageTextInput.tsx
+++ Components/MessageTextInput.tsx
@@ -25,7 +25,7 @@ const MessageTextInput = ({
       <div className="relative">
         <input
           id="message"
-          className="block w-full p-4 ps-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-green-400"
+          className="block w-full p-4 ps-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
           placeholder="Message VultGPT..."
           value={input}
           onChange={onChange}
@@ -33,12 +33,12 @@ const MessageTextInput = ({
         <button
           type="submit"
           onClick={onClick}
-          className={`absolute px-2 py-1 text-sm font-medium text-center inline-flex items-center text-white rounded-lg focus:ring-4 focus:outline-none focus:ring-green-400 message-submit-button ${
+          className={`absolute px-2 py-1 text-sm font-medium text-center inline-flex items-center text-white rounded-lg message-submit-button ${
             input.length > 0 ? "bg-slate-800" : "bg-slate-300"
           }`}
         >
           <svg
-            className="w-6 h-6 text-white dark:text-white"
+            className="w-6 h-6 text-white"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -48,7 +48,7 @@ const MessageTextInput = ({
               stroke="currentColor"
               stroke-linecap="round"
               stroke-linejoin="round"
-              stroke-width="2"
+              stroke-width="1"
               d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
             />
           </svg>
